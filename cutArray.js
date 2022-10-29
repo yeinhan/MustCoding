cb50e3fee303151a1e1168e76db8e7132dd2392a
yeinHan <>
2022-10-29 20:17:13 +0900

    MustCoding 1주차 문제2번 배열 자르기

--- cutArray.js
+++ cutArray.js
@@ -1,1 +1,11 @@
-console.log("dd")
+function solution(numbers, num1, num2) {
+    var answer = [];
+    
+    for(var i=num1; i<=num2; i++){
+        answer.push(numbers[i]);
+    }
+    return answer;
+}
+
+console.log(solution([1,2,3,4,5], 1, 3));
+console.log(solution([1,3,5], 1, 2));
