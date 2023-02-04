2d8dab6c8bc35e83d5013762fffeae969da5ff77
yeinhan <goregore8637@naver.com>
2023-02-05 02:25:33 +0900

    그냥 재미로 품

---
+++ sumOfDivisors.js
@@ -0,0 +1,15 @@
+function solution(n){
+
+    var answer = 0;
+
+    for(var i=1; i<=n-1; i++){
+        if(n % i == 0){
+            answer += i;
+        }
+    }
+
+    return answer + n;
+}
+
+console.log(solution(12))
+console.log(solution(5))
